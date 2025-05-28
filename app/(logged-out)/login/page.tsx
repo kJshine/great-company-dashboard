5bb1ce8232b336880a08a3b8f4972a65fb5630b9
devJaehwan <fodore12@gmail.com>
2025-05-28 15:23:38 +0900

    feat: 로그인 페이지 추가

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -4,24 +4,88 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { PersonStandingIcon } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 export default function LoginPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <>
-      <PersonStandingIcon size={50} />
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle>로그인</CardTitle>
-          <CardDescription>로그인 후 이용해주세요.</CardDescription>
+          <CardTitle className="text-2xl">로그인</CardTitle>
         </CardHeader>
-        <CardContent>로그인폼</CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="이메일을 입력해주세요." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">로그인</Button>
+            </form>
+          </Form>
+        </CardContent>
         <CardFooter className="flex justify-between">
           <small>계정이 없으신가요?</small>
           <Button asChild variant="outline" size="sm">
