966f3f81e1335ca4fc07e311b0f958b67249878b
devJaehwan <fodore12@gmail.com>
2025-06-18 18:35:02 +0900

    feat: 테스트 코드 resize 오류 수정

--- setupTests.ts
+++ setupTests.ts
@@ -1,1 +1,16 @@
 import "@testing-library/jest-dom";
+
+global.ResizeObserver = class ResizeObserver {
+  constructor(callback) {
+    this.callback = callback;
+  }
+  observe() {
+    // mock implementation
+  }
+  unobserve() {
+    // mock implementation
+  }
+  disconnect() {
+    // mock implementation
+  }
+};
